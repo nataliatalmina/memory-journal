e834e691580f5c5b07b5f56e5292416c88a04e95
Natalia Talmina <natalia.talmina@gmail.com>
2026-06-05 12:25:22 +0100

    Initial Commit

---
+++ MemoryJournal/MemoryJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryJournalApp.swift
+//  MemoryJournal
+//
+//  Created by Natalia Talmina on 05/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MemoryJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
